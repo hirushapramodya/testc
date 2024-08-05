3202d3f69a76ca3b9fb58af15148324a269b8eda
hirushapramodya <hirushapramodya221@gmail.com>
2024-08-05 18:09:13 +0530

    Edited/simple.js

---
+++ example.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class example{
+    public static void main(String[] args) {
+        ArrayList <Integer> abc = new ArrayList();
+        abc.add(10);
+        abc.add(20);
+        abc.add(30);
+        abc.add(40);
+        abc.add(50);
+        System.out.println(abc);
+    }
+}
