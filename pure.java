b0f98949a85d6b4ac043fb304e96f8171d0aa8c2
hirushapramodya <hirushapramodya221@gmail.com>
2024-08-05 21:24:23 +0530

    Edited/pure.java

--- pure.java
+++ pure.java
@@ -1,5 +1,8 @@
 class pure{
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hiruhsa");
+        System.out.println("pramodya");
+        System.out.println("samarasinghe");
     }
 }
